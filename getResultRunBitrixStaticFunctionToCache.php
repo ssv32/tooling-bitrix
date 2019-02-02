8ee06977f72bbec0e3772a4f0447c5ae112ec62f
ssv32 <ssv_32@mail.ru>
2019-02-02 23:13:28 +0300

    fix text comment function getResultRunBitrixStaticFunctionToCache()

--- getResultRunBitrixStaticFunctionToCache.php
+++ getResultRunBitrixStaticFunctionToCache.php
@@ -1,10 +1,12 @@
 <?
 /** 
- *  getResultRunBitrixStaticFunctionToCache
+ *  getResultRunBitrixStaticFunctionToCache - функция выполнит статичный метод класса битрикс
+ *  	и закэширует результат. (название класса, метода и параметров нужно передать)
+ * 	цель - не создавать много методов для получения всякого и кэширование полученного а использовать эту функцию.
  *  @param $Class - класс
  *  @param $function - метод
  *  @param $property - параметры метода
- *  @param int $cacheTime время кеширования (может быть false), 24ч. по умолчанию
+ *  @param int $cacheTime время кэширования (может быть false), 24ч. по умолчанию
  *	@return array
  */
 function getResultRunBitrixStaticFunctionToCache($Class, $function, $property, $cacheTime = 86400   ){
